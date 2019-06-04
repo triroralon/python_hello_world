c456d10d9d258970051ac800b1199821d9f9344e
robert.messer <rob3585@hotmail.com>
2019-06-03 17:19:25 -0700

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,30 @@
+# 1. TASK: print "Hello World"copy
+print( "hello world" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print( "hello", name,"!")	# with a comma
+print("hello" + name +"!")	# with a +
+# 3. print "Hello 42!" with the number in a variable
+age = 42
+print("hello", age,"!")	# with a comma
+# print("hello" + age + "!")	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1,fave_food2)) # with .format()
+print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
+
+first_name = "Robert"
+print("Hello", first_name, "!")
+print("Hello " + first_name + "!")
+print(f"Hello {first_name}!")
+fav_num = "55"
+print("Hello", fav_num,"!")
+print("hello" + fav_num +"!")
+
+fave_food1 = "pizza"
+fave_food2 = "spegetti"
+print(f"I love to eat {fave_food1} and {fave_food2}.")
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+
+
